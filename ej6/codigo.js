371c8f527285f8d97480b8b227e8b089ac50f6d5
Unknown <jomaroru@hotmail.com>
2018-09-26 23:25:21 +0200

    ejercicio6

---
+++ ej6/codigo.js
@@ -0,0 +1,15 @@
+let letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+
+let numero = prompt("DNI sin letra: ");
+let letra = prompt("Letra DNI");
+letra = letra.toUpperCase();
+if ((parseInt(numero)%99999999) < 1 || parseInt(numero) > 99999999) {
+  alert("El número proporcionado no es válido");
+} else {
+  let letraCorrecta = letras[numero % 23];
+  if (letraCorrecta != letra) {
+    alert("ERROR");
+  } else {
+    alert("DNI Correcto");
+  }
+}
