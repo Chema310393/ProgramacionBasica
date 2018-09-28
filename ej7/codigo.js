423e0d8b884d1d9c9f54c3a510be7d653b133962
Unknown <jomaroru@hotmail.com>
2018-09-28 10:57:11 +0200

    prueba

--- ej7/codigo.js
+++ ej7/codigo.js
@@ -5,6 +5,6 @@
   for (let index = numero; index > 0; index--) {
     factorial *= index;
   }
-
+  print('El factorial de ' + numero + ' es ' + factorial);
   alert('El factorial de ' + numero + ' es ' + factorial);
 }
