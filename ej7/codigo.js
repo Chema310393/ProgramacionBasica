8560148990c31583dd44d232e01afe6ca1c61b15
Unknown <jomaroru@hotmail.com>
2018-09-28 08:29:21 +0200

    ejercicio 7

---
+++ ej7/codigo.js
@@ -0,0 +1,8 @@
+let numero, factorial=1;
+
+numero=prompt('Introduzca un número: ');
+for (let index = numero; index >0 ; index--) {
+  factorial*=index;  
+}
+
+alert('El factorial de '+numero+' es '+factorial);
